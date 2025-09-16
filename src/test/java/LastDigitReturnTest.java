7db506c1fe6a6fb265fc3400839e70488db16593
Prasuna Chandrika K <prasunachandrika156@gmail.com>
2025-09-16 15:08:13 +0530

    commit done

---
+++ src/test/java/LastDigitReturnTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LastDigitReturnTest {
+	@Test
+	public void testCase1() {
+		Assertions.assertEquals(6,LastDigitReturn.lastDigitReturn(156));
+	}
+	@Test
+	public void testCase2() {
+		Assertions.assertEquals(6,LastDigitReturn.lastDigitReturn(-156));
+	}
+}
